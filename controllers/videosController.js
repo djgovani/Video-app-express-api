1020072c47054ca53b227fd90ae0d54898c645f3
djgovani <deepgovani97@gmail.com>
2018-07-05 12:27:46 +0530

    Added update and get Single video controller

--- controllers/videosController.js
+++ controllers/videosController.js
@@ -8,37 +8,19 @@ var path = require('path');
 const file = path.join(__dirname, '../database/video.json');
 
 module.exports = {
-  getVideos(req, res) {
-    fs.exists(file, function(exists){
-      if(exists){
-        fs.readFile(file, (err, data) => {
-          if (err) {
-            console.log(err);
-            res.status(500).json({err});
-          }
-          if (data) {
-            console.log(data)
-            const parsedData = JSON.parse(data);
-            res.status(200).json({
-              data: parsedData,
-            });
-          }
-        });
-      } else {
-        res.status(204).json({message: 'file not exists'});
-        console.log("file not exists");
-      }
-    });
-  },
-
+  /*
+  ----------------------------------------------------------------------
+    - postVideos Controller Function
+      * Input: id, title and category_id
+      * Output: 201 status code on Success, Other Status Code on Fail
+  ----------------------------------------------------------------------
+  */
   postVideos(req, res) {
     if (req.body != null) {
       const id = req.body.id;
       const title = req.body.title;
       const category_id = parseInt(req.body.category_id, 0);
-      let obj = {
-         data: []
-      };
+      let obj;
       fs.exists(file, function(exists){
         if(exists){
           fs.readFile(file, (err, data) => {
@@ -79,60 +61,130 @@ module.exports = {
     }
   },
 
-  // updateVideos(req, res) {
-  //   if (req.body != null) {
-  //     const id = req.body.id;
-  //     const title = req.body.title;
-  //     const category_id = parseInt(req.body.category_id, 0);
-  //     let obj = {
-  //        data: []
-  //     };
-  //     fs.exists(file, function(exists){
-  //       if(exists){
-  //         fs.readFile(file, (err, data) => {
-  //           if (err) {
-  //             console.log(err);
-  //             return res.status(500).json({err}).end();
-  //           }
-  //           obj = JSON.parse(data);
-  //           let notExists = true;
-  //           for (let i=0; i <= obj.video.length - 1; i++) {
-  //             if (obj.video[i].id === id && obj.video[i].title === title && obj.video[i].category_id === category_id) {
-  //               return res.status(409).json({message: 'Already exists'}).end();
-  //               notExists = false;
-  //             } else {
-  //               notExists = true;
-  //             }
-  //           }
+  /*
+  ----------------------------------------------------------------------
+    - updateVideos Controller Function
+      * Input: id
+      * Output: 200 status code on Success, Other Status Code on Fail
+  ----------------------------------------------------------------------
+  */
+  updateVideos(req, res) {
+    if (req.body != null) {
+      const id = req.body.id;
+      const title = req.body.title;
+      const category_id = parseInt(req.body.category_id, 0);
+      let obj;
+      fs.exists(file, function(exists){
+        if(exists){
+          fs.readFile(file, (err, data) => {
+            if (err) {
+              console.log(err);
+              return res.status(500).json({err}).end();
+            }
+            obj = JSON.parse(data);
+            let notExists = true;
+            for (let i=0; i <= obj.video.length - 1; i++) {
+              if (obj.video[i].id === id && obj.video[i].title === title && obj.video[i].category_id === category_id) {
+                return res.status(409).json({message: 'Already exists'}).end();
+                notExists = false;
+              } else {
+                notExists = true;
+              }
+            }
 
-  //           if (notExists) {
-  //             let index;
-  //             for (let i=0; i <= obj.video.length - 1; i++) {
-  //               if (obj.video[i].id === id) {
-  //                 delete obj.video.indexOf(id);
+            if (notExists) {
+              let index;
+              for (let i=0; i <= obj.video.length - 1; i++) {
+                if (obj.video[i].id === req.params.id) {
+                  obj.video[i].id = id;
+                  obj.video[i].title = title;
+                  obj.video[i].category_id = category_id;
+                }
+              }
+              json = JSON.stringify(obj);
+              fs.writeFile(file, json, 'utf8', (err, response) => {
+                if (err) {
+                  console.log(err);
+                  return res.status(500).json({err}).end();
+                } else {
+                  return res.status(200).json({message: 'Video updated.'}).end();
+                }
+              });
+            }
+          });
+        } else {
+          return res.status(404).json({message: 'file not exists'}).end();
+        }
+      });
+    } else {
+      return res.status(204).json({message: 'No content'}).end();
+    }
+  },
 
-  //                 // delete
-  //                 console.log('deleted', obj.video);
-  //               }
-  //             }
-  //             obj.video.push({id: id, title: title, category_id: category_id});
-  //             json = JSON.stringify(obj);
-  //             fs.writeFile(file, json, 'utf8', (err, response) => {
-  //               if (err) {
-  //                 console.log(err);
-  //                 return res.status(500).json({err}).end();
-  //               } else {
-  //                 return res.status(201).json({message: 'Video updated.'}).end();
-  //               }
-  //             });
-  //           }
-  //         });
-  //       } else {
-  //         return res.status(404).json({message: 'file not exists'}).end();
-  //       }
-  //     });
-  //   } else {
-  //     return res.status(204).json({message: 'No content'}).end();
-  //   }
-  // }
+  /*
+  ----------------------------------------------------------------------
+    - getVideos Controller Function
+      * Output: 200 status code on Success, videos
+  ----------------------------------------------------------------------
+  */
+  getVideos(req, res) {
+    fs.exists(file, function(exists){
+      if(exists){
+        fs.readFile(file, (err, data) => {
+          if (err) {
+            console.log(err);
+            res.status(500).json({err});
+          }
+          if (data) {
+            console.log(data)
+            const parsedData = JSON.parse(data);
+            res.status(200).json({
+              data: parsedData.video,
+            });
+          }
+        });
+      } else {
+        res.status(204).json({message: 'file not exists'});
+        console.log("file not exists");
+      }
+    });
+  },
+
+  /*
+  ----------------------------------------------------------------------
+    - getSingleVideo Controller Function
+      * Output: 200 status code on Success, video
+  ----------------------------------------------------------------------
+  */
+  getSingleVideo(req, res) {
+    const id = req.params.id;
+    fs.exists(file, function(exists){
+      if(exists){
+        fs.readFile(file, (err, data) => {
+          if (err) {
+            console.log(err);
+            res.status(500).json({err});
+          }
+          if (data) {
+            const obj = JSON.parse(data);
+            let videoFound = false;
+            for (let i=0; i <= obj.video.length - 1; i++) {
+              if (obj.video[i].id === req.params.id) {
+                videoFound = true;
+                return res.status(200).json({
+                  data: obj.video[i].id,
+                }).end();
+              }
+            }
+            if (!videoFound) {
+              res.status(404).json({message: 'Not found'}).end();
+            }
+          }
+        });
+      } else {
+        res.status(204).json({message: 'file not exists'});
+        console.log("file not exists");
+      }
+    });
+  }
 };
