b6c6d872d2fff3ef92eb341d7da1098cbbdecc67
djgovani <deepgovani97@gmail.com>
2018-07-05 18:27:14 +0530

    deploy

--- app.js
+++ app.js
@@ -16,10 +16,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'database')));
 app.disable('etag');
 
-app.use((err, req, res, next) => {
-  //some security mechanism headers to avoid errors in browser
+app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
   next();
 });
 app.use('/', indexRouter);
